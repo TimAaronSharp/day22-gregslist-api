0740c7bd5f19691497e9cddc1e462d73c1d2b690
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-03-11 17:43:58 -0600

    getJobByID in JobsService complete

--- src/services/JobsService.js
+++ src/services/JobsService.js
@@ -1,3 +1,4 @@
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js";
 import { dbContext } from "../db/DbContext.js";
 
 
@@ -6,6 +7,17 @@ class JobsService {
   async getAllJobs() {
     const jobs = await dbContext.Jobs.find()
     return jobs
+  }
+  async getJobsByQuery(jobQuery) {
+
+  }
+  async getJobById(jobId) {
+    const job = await dbContext.Jobs.findById(jobId)
+
+    if (job == null) {
+      throw new BadRequest(`${jobId} is not a valid job id, you MORON!!!`)
+    }
+    return job
   }
 }
 
