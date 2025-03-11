f75c44dc6815d4d2aefe4242b5a99153b2955dcd
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-03-11 17:40:28 -0600

    getJobsByQuery and getJobById created in JobsController

--- src/controllers/JobsController.js
+++ src/controllers/JobsController.js
@@ -7,6 +7,8 @@ export class JobsController extends BaseController {
     super('api/jobs')
     this.router
       .get('', this.getAllJobs)
+      .get('/search', this.getJobsByQuery)
+      .get('/:jobId', this.getJobById)
   }
   /**
      * @param {import("express").Request} req
@@ -22,5 +24,32 @@ export class JobsController extends BaseController {
     }
 
   }
-
+  /**
+       * @param {import("express").Request} req
+       * @param {import("express").Response} res
+       * @param {import("express").NextFunction} next
+       */
+  async getJobsByQuery(req, res, next) {
+    try {
+      const jobQuery = req.query
+      const jobs = await jobsService.getJobsByQuery(jobQuery)
+      res.send(jobs)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**
+       * @param {import("express").Request} req
+       * @param {import("express").Response} res
+       * @param {import("express").NextFunction} next
+       */
+  async getJobById(req, res, next) {
+    try {
+      const jobId = req.params.jobId
+      const job = await jobsService.getJobById(jobId)
+      res.send(job)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
