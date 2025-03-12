0e0f65b0f04286ace5ff123e9170b8cef0a955ef
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-03-11 18:51:47 -0600

    getPetsByQuery in PetsService complete with .find, .limit, .skip, .sort, and .populate

--- src/services/PetsService.js
+++ src/services/PetsService.js
@@ -8,7 +8,34 @@ class PetsService {
     return pets
   }
   async getPetsByQuery(petQuery) {
+    const pageNumber = parseInt(petQuery.page) || 1
+    const petLimit = 5
+    const skipAmount = pageNumber * petLimit - petLimit
+    delete petQuery.page
 
+    const sortBy = petQuery.sort
+    delete petQuery.sort
+    const petsCount = await dbContext.Pets.countDocuments(petQuery)
+    const totalPages = Math.ceil(petsCount / petLimit) || 1
+
+    if (pageNumber > totalPages) {
+      throw new BadRequest(`${pageNumber} is greater than the total amount of pages (${totalPages}), you MORON!!!`)
+    }
+
+    const pets = await dbContext.Pets
+      .find(petQuery)
+      .limit(petLimit)
+      .skip(skipAmount)
+      .sort(sortBy)
+      .populate('creator')
+
+    const responseObj = {
+      currentPage: pageNumber,
+      pets: pets,
+      totalPets: petsCount,
+      totalPages: totalPages
+    }
+    return responseObj
   }
   async getPetById(petId) {
     const pet = await dbContext.Pets.findById(petId)
